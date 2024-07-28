d41a3d70f768bda0405590d28327cffbaf0c82e7
Mohamed Ridha <hamodeygame45@gmail.com>
2024-07-28 23:50:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,30 @@
-from telebot import *
-from mindai import *
-from types import *
-bot = TeleBot("7202104518:AAFeZK4Dz9GclJKV0kXOG1Vr9jY3BhPazzU")
-#توكن بوتك 
+import telebot
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+
+# استبدل TOKEN بالتوكن الخاص بالبوت الخاص بك
+TOKEN = '7202104518:AAFeZK4Dz9GclJKV0kXOG1Vr9jY3BhPazzU'
+bot = telebot.TeleBot(TOKEN)
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    userm = message.from_user.username
-    ID = message.from_user.id
-    name_pro = message.from_user
-    key = types.InlineKeyboardMarkup()
-    bot1 = types.InlineKeyboardButton('- Dev 💣' ,url ='https://t.me/Class_zaid')
-    bot2 = types.InlineKeyboardButton('- Channel 🔥' ,url ='https://t.me/ZeusThon')
-    key.add(bot1,bot2)
-    p1ng = "https://t.me/ZeusThon/7038"
-    bot.send_photo(message.chat.id,p1ng,f"""<strong>
- English :- 
-- Welcom @{userm} .
-- Yuor Id {ID}
-- Send Yuo And Bot .
-- Start Now .
-* - - - - - - - - - - - - - - - - -*
-Arbic :  
-- اهلا بك عزيزي @{userm}💣
-تحدث مع البوت بلغه الانكليزي فقط 
-ابدء الان .
-</strong>""",parse_mode="html",reply_markup=key)
+    bot.reply_to(message, "🔼 مرحبًا! لإعداد الجلسة، يرجى إدخال APP ID و API HASH.")
+
 @bot.message_handler(func=lambda message: True)
-def main(message):
-    x = Ai().chat(message=message.text)
-    bot.reply_to(message,x['reply'])
-bot.infinity_polling()
+def get_credentials(message):
+    try:
+        # الحصول على APP ID و API HASH
+        app_id = int(message.text.split()[0])
+        api_hash = message.text.split()[1]
+
+        with TelegramClient(StringSession(), app_id, api_hash) as client:
+            session_str = client.session.save()
+            bot.send_message(message.chat.id, session_str)
+            bot.send_message(message.chat.id, "🔼 هذا هو كود التيرمكس الخاص بك لا تعطيه لأي شخص لان معرض للاختراق ❤️ @Scorpions_scorp")
+            print("تم إرسال StringSession إلى المحادثة.")
+
+    except Exception as e:
+        bot.reply_to(message, f"حدث خطأ: {str(e)}. تأكد من إدخال APP ID و API HASH بشكل صحيح.")
+
+# بدء تشغيل البوت
+bot.polling()
