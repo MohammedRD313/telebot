c3566541a21093d9ff6dcdb445eb80ce8ddceb4c
Mohamed Ridha <hamodeygame45@gmail.com>
2024-07-30 13:33:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,89 +1,45 @@
-import os
-from telebot import types
-import telebot
-import requests
-import random
+#By Team Aljoker For You ❤️🤡
 
+from telethon import TelegramClient, events, Button
+import subprocess
+from random import choices, randint
 
-token = '7231254366:AAHkHtHowT9Sd5iBVJNTqNMdN51uvLPrGsk'
-#هنا توكنك داخل دبل كتيشن
+api_id = '23452838'
+api_hash = '48333ea197122c79561e1321a0687076'
+bot_token = '7486893779:AAE8jq5XIW3HWhTkYDiq5_Lnin_B7R-68Ig'
+Hussein = TelegramClient('aljoker_session', api_id, api_hash).start(bot_token=bot_token)
 
-bot = telebot.TeleBot(token)
+@Hussein.on(events.NewMessage(pattern='/start'))
+async def aljoker(event):
+    keyboard = [[Button.inline('إنشاء قاعدة بيانات', b'aljoker_postgres')]]
+    await event.reply(
+        f'''**اهلاً وسهلاً حبيبي {event.sender.first_name}،
+‎لإنشاء قاعدة بيانات خاصة بسورس الجوكر قم بالضغط على زر إنشاء قاعدة بيانات**''',
+        buttons=keyboard
+    )
 
-bo = types.InlineKeyboardButton(text='Start Bot', callback_data='start')
-me = types.InlineKeyboardButton(text='Channel', url='https://t.me/ttxxxn')
+@Hussein.on(events.CallbackQuery)
+async def handle_callback(event):
+    if event.data == b'aljoker_postgres':
+        OHussein = ''.join(choices('abcdefghijklmnopqrstuvwxyz0123456789', k=randint(5, 10)))
+        await event.respond('**᯽︙ انتظرني أسوي لك قاعدة بيانات لعيونك🥰**')
 
-@bot.message_handler(commands=['start'])
-def start(message):
-	m = types.InlineKeyboardMarkup(row_width=2)
-	m.add(bo, me)
-	bot.send_message(message.chat.id, f"<b>Hi 🥰 > {message.from_user.first_name} .</b>",parse_mode='html',reply_markup=m)
+        create_user_aljoker = f'sudo su - postgres -c "psql -c \\"CREATE USER joker{OHussein} WITH PASSWORD \'joker{OHussein}\';\\""'
+        create_db_aljoker = f'sudo su - postgres bash -c "createdb joker{OHussein} -O joker{OHussein}"'
 
-@bot.callback_query_handler(func=lambda call: True)
-def qwere(call):
-	if call.data == 'start':
-		li(call.message)
+        create_user_process = subprocess.Popen(create_user_aljoker, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        user_res, user_err = create_user_process.communicate()
 
-def li(message):
-    error = 0
-    done = 0
-    no=0
-    koko = bot.reply_to(message, "<b>Start fishing ...⌛</b>", parse_mode='html').message_id
+        if 'CREATE ROLE' in user_res.decode():
+            create_db_process = subprocess.Popen(create_db_aljoker, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            db_res, db_err = create_db_process.communicate()
 
-    while True:
-        us = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_1234567890'
-        user1 = ("".join(random.choice(us)for i in range(1)))
-        user2 = ("".join(random.choice(us)for i in range(1)))
-        user = user1+user1+user1+user1+user1+user2
-        
-        req = requests.get(f'https://t.me/{user}').text
-        if '"robots"' in req:
-        	headers = {
-    'authority': 'fragment.com',
-    'accept': 'application/json, text/javascript, */*; q=0.01',
-    'accept-language': 'ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-     'cookie': 'stel_ssid=b2a6d1116a8f8a31fb_1331735887277205031; stel_dt=-180',
-    'referer': 'https://fragment.com/?query=py_50',
-    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-    'x-aj-referer': 'https://fragment.com/?query=py_50',
-    'x-requested-with': 'XMLHttpRequest'}
-
-        	r = requests.get(f'https://fragment.com/username/{user}',headers=headers).text
-        	if '?query' in r:
-        	   done += 1
-        	   bot.send_message(message.chat.id, f"<b>Good user\nuser : @{user}</b>", parse_mode="html")  
-        	else:
-        		error += 1
-        		
+            if not db_err:
+                await event.respond(f'''**وهاي قاعدة البيانات وتدلل علينا 😘 : `postgresql://joker{OHussein}:joker{OHussein}@localhost:5432/joker{OHussein}`**''')
+            else:
+                await event.respond(f'حدث خطأ أثناء إنشاء قاعدة البيانات:\n{db_err.decode()}')
         else:
-        	no += 1
-        	   
-        	ms = types.InlineKeyboardMarkup(row_width=2)
-        	
-        	v1 = types.InlineKeyboardButton(f"Hit ➜ [ {done} ]", callback_data='mn')
-        	v2 = types.InlineKeyboardButton(f"Bad ➜ [ {error} ]", callback_data='mnm')
-        	Checkr = types.InlineKeyboardButton(f"user ➜ [ @{user} ]", callback_data='asnm')
-        	nno = types.InlineKeyboardButton(f"Erorr ➜ [ {no} ]", callback_data='asm')
-        	v3 = types.InlineKeyboardButton(f"@PY_50", url="https://t.me/PY_50", callback_data='tynn')
-        	v4 = types.InlineKeyboardButton(f"@ttxxxn", url="https://t.me/ttxxxn", callback_data='mnn')
-        	ms.add(v1,v2)
-        	ms.add(Checkr,nno)
-        	ms.add(v4,v3)
-        	
-        	
-        	bot.edit_message_text(chat_id=message.chat.id, message_id=koko, text='<b>Start Fishing By @PY_50 ....⌛</b>', parse_mode='html', reply_markup=ms)
-            
-        
+            await event.respond(f'حدث خطأ أثناء إنشاء المستخدم:\n{user_err.decode()}')
 
-print(f'Go Bot starting \ntoken : {token}')
-
-bot.polling(True)
-
-
-#حسو ال علي
+print("البوت يشتغل استمتع 😍...")
+Hussein.run_until_disconnected()
